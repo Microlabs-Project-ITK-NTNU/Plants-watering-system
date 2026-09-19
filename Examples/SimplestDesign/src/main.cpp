f377d94ec7d332af6f163576732ed33c58498a88
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-09-19 14:02:53 +0200

    Changed comments for better explanations

--- Examples/SimplestDesign/src/main.cpp
+++ Examples/SimplestDesign/src/main.cpp
@@ -1,30 +1,32 @@
-#include <Arduino.h>
+// Includes the Arduino library, so you can use their functions
+#include <Arduino.h>  
 
-// put function declarations here:
+// Set pin numbers for IO:
 const int analogPin = 36;
 const int relayPin = 32;
 
+// Set threshold for relay activation
 const int threshold = 1000;
 
+// Initialize the value variable outside the loop, to save performance 
 int val = 0;
 
+// Setup runs automatically, when using the Arduino framework
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); // Baudrate for the serial monitor
   pinMode(relayPin, OUTPUT); // Sets the digital relay pin as output
 }
 
+// Loop will run again and again, when using the Arduino framework
 void loop() {
-  // put your main code here, to run repeatedly:
-  val = analogRead(analogPin);
-  Serial.println(val);
+  val = analogRead(analogPin); // Reads analog value of sensor, using arduino function for ADC
+  Serial.println(val); // Prints the value to the serial monitor
   
   if (val < threshold) {
-    // pump vann
-    digitalWrite(relayPin, HIGH); // Writes to the relay pin
+    digitalWrite(relayPin, HIGH); // Writes relay pin high, starts pump
   } else if (val >= threshold) {
-    digitalWrite(relayPin, LOW);
+    digitalWrite(relayPin, LOW); // Writes relay pin low, stops pump
   }
 
-  delay(200);
+  delay(200); // Waits .2 seconds, mostly to not overflow the serial monitor
 }
